9dc2b4740dda1508e548af7f16abb806a3f214b9
Nick Wise <nickcwise@gmail.com>
2017-06-28 15:00:22 -0400

    Exercise Added

--- Conditionals1/Program.cs
+++ Conditionals1/Program.cs
@@ -11,8 +11,8 @@ namespace Conditionals1
         static void Main(string[] args)
         {
 
-            
 
+            /*
             Console.WriteLine("Thank you for eating at RESTAURANT");
             
             //float in case of decimal total
@@ -44,9 +44,94 @@ namespace Conditionals1
                 billSplit = discountFive / numberOfPeople;
                 Console.WriteLine("Each person in your party will pay $ " + billSplit);
             }
-            
-            
+            */
 
+            //Console.WriteLine("Welcome to the Age Distribution Center. This program will determine how useful you are to society.");
+            //Console.WriteLine("Please enter your age:");
+
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= 0 && age <= 2)
+            //{
+            //    Console.WriteLine("You are Still in Mama's Arms and therefore useless");
+            //}
+            //else if (age >= 3 && age <= 4)
+            //{
+            //    Console.WriteLine("You are a Preschool Maniac");
+            //}
+            //else if (age >= 5 && age <= 11)
+            //{
+            //    Console.WriteLine("You are in Elementary School and are a nuisance to others");
+            //}
+            //else if (age >= 12 && age <= 14)
+            //{
+            //    Console.WriteLine("You are in Middle School and are a mischevious miscreant");
+            //}
+            //else if (age >= 15 && age <= 18)
+            //{
+            //    Console.WriteLine("You are in High School and no one wants to deal with you.");
+            //}
+            //else if (age >= 19 && age <= 22)
+            //{
+            //    Console.WriteLine("You are in College. Good luck!");
+            //}
+            //else if (age >= 23 && age <= 65)
+            //{
+            //    Console.WriteLine("You are Working for the Man... FOREVER! Maybe consider going to We Can Code IT");
+            //}
+            //else if (age >= 66 && age <= 100)
+            //{
+            //    Console.WriteLine("You are in the 'Golden Years'");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are either extremely old or an extraterrestrial, this program is for humans.");
+            //}
+
+            //Switch Case Exercise
+
+            Console.WriteLine("Please type out any number: ");
+            string wordNumber = (Console.ReadLine());
+
+            switch (wordNumber)
+            {
+                case "Zero":
+                    Console.WriteLine("0");
+                    break;
+                case "One":
+                    Console.WriteLine("1");
+                    break;
+                case "Two":
+                    Console.WriteLine("2");
+                    break;
+                case "Three":
+                    Console.WriteLine("3");
+                    break;
+                case "Four":
+                    Console.WriteLine("4");
+                    break;
+                case "Five":
+                    Console.WriteLine("5");
+                    break;
+                case "Six":
+                    Console.WriteLine("6");
+                    break;
+                case "Seven":
+                    Console.WriteLine("7");
+                    break;
+                case "Eight":
+                    Console.WriteLine("8");
+                    break;
+                case "Nine":
+                    Console.WriteLine("9");
+                    break;
+                case "Ten":
+                    Console.WriteLine("10");
+                    break;
+                default:
+                    Console.WriteLine("Nope");
+                    break;
+            }
         }
 
 
